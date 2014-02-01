1a6e4f99b49a9893bf47ebf0a96e5e048181740f
Jacob Hughes <exrook.j@gmail.com>
2014-02-01 18:27:30 -0500

    Changed to tank drive, added support for running the wheels Transitioned to 1TBS

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -6,15 +6,18 @@
  * Autonomous and OperatorControl methods at the right time as controlled by the switches on
  * the driver station or the field controls.
  */ 
-class RobotDemo : public SimpleRobot
-{
+class RobotDemo : public SimpleRobot {
 	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+	Joystick left, right, func; //only joystick
+	Victor ballMotor;
 
 public:
 	RobotDemo():
 		myRobot(1, 2),	// these must be initialized in the same order
-		stick(1)		// as they are declared above.
+		left(1),		// as they are declared above.
+		right(2),
+		func(2),
+		ballMotor(7) 
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -22,23 +25,26 @@ public:
 	/**
 	 * Drive left & right motors for 2 seconds then stop
 	 */
-	void Autonomous()
-	{
-		myRobot.SetSafetyEnabled(false);
+	void Autonomous() {
+		/*myRobot.SetSafetyEnabled(false);
 		myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
 		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
+		myRobot.Drive(0.0, 0.0); 	// stop robot*/
 	}
 
 	/**
 	 * Runs the motors with arcade steering. 
 	 */
-	void OperatorControl()
-	{
+	void OperatorControl() {
 		myRobot.SetSafetyEnabled(true);
-		while (IsOperatorControl())
-		{
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+		while (IsOperatorControl()) {
+			myRobot.TankDrive(left.GetY(),right.GetY()); // drive with arcade style (use right stick)
+			if (func.GetRawButton(1)) {
+				ballMotor.Set(0.5);
+				//printf("Function: %f",func.GetZ());
+			} else {
+				ballMotor.Set(0.0);
+			}
 			Wait(0.005);				// wait for a motor update time
 		}
 	}
