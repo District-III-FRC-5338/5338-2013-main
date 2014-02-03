e88a5dfcc4517fae1dcc40bc612c7b977dc77549
Jacob Hughes <exrook.j@gmail.com>
2014-02-02 20:53:30 -0500

    Added motor stop button for secondary driver on button 5 Changed precision mode to turbo mode due to a serious accident involving precision mode Added a straight driving mode by pressing the trigger on the right drive stick Added skeleton code for the pickup mechanism on buttons 2 and 3 Changed all tabs to 2 width spaces

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -8,79 +8,95 @@
 //Defines for each controller
 #DEFINE LEFTJ 1
 #DEFINE RIGHTJ 2
-#DEFINE FUNCJ 2
+#DEFINE FUNCJ 3
 //Defines for each motor
 #DEFINE DRIVEMOTORL 1
 #DEFINE DRIVEMOTORR 2
 #DEFINE BALLMOTOR 7
 
 class RobotDemo : public SimpleRobot {
-	RobotDrive myRobot; // robot drive system
-	Joystick left, right, func; // only joystick
-	Victor ballMotor;
-	
+  RobotDrive myRobot; // robot drive system
+  Joystick left, right, func; // only joystick
+  Victor ballMotor;
+  
 public: // These Methods can be accesed by other code
-	
-	//The constructor for the class, it is run before any other code
-	//it uses an initializer list to initalize variables used by this class
-	
-	RobotDemo(): 
-		myRobot(DRIVEMOTORL, DRIVEMOTORR),  // init drive system with both motors
-		left(LEFTJ),  // init joysticks, left drive,
-		right(RIGHTJ),// right drive,
-		func(FUNCJ),  // other functions
-		ballMotor(BALLMOTOR) { // Motor used to bring in the ball
-		myRobot.SetExpiration(0.1);
-	}
-	
-	// This code runs during autonomous
-	// It does nothing right now
-	
-	void Autonomous() {
-		//We haven't programmed autonomous yet
-		
-		myRobot.SetSafetyEnabled(false);
-	}
-	
-	// This code runs during the tele-operated period
-	// Runs the motors with tank steering
-	// Also runs the ball pickup motor when the tigger is pressed
-	
-	void OperatorControl() {
-		
-		myRobot.SetSafetyEnabled(true);
-		
-		float scaleFactor; // Factor to scale the joystick values by
-		float leftpow,rightpow; // Power for the left and right motors
-		
-		while (IsOperatorControl()) {
-		  
-		  if (left.GetRawButton(1)) // if the left trigger is held
-		    scaleFactor = 0.5; // scale the speed by half, allows for precision movement
-		  else
-		    scaleFactor = 1.0; // else don't scale, allows for speed
-		  
-		  //set the power for the to the Y-axes of the Joystick multiplied by the scale factor
-		  leftpow = left.getY()*scaleFactor;
-		  rightpow = right.getY()*scaleFactor;
-		  
-			myRobot.TankDrive(leftpow,rightpow); // drive tank style
-			
-			if (func.GetRawButton(1)) // if the trigger on the function joystick is pressed
-				ballMotor.Set(0.5); // run the ball pickup motor
-			else
-				ballMotor.Set(0.0); // else don't run the motor
-			
-			Wait(0.005);				// wait for a motor update time
-		}
-	}
-	
-	/**
-	 * Runs during test mode
-	 */
-	void Test() {
+  
+  //The constructor for the class, it is run before any other code
+  //it uses an initializer list to initalize variables used by this class
+  
+  RobotDemo(): 
+    myRobot(DRIVEMOTORL, DRIVEMOTORR),  // init drive system with both motors
+    left(LEFTJ),  // init joysticks, left drive,
+    right(RIGHTJ),// right drive,
+    func(FUNCJ),  // other functions
+    ballMotor(BALLMOTOR) { // Motor used to bring in the ball
+    myRobot.SetExpiration(0.1);
+  }
+  
+  // This code runs during autonomous
+  // It does nothing right now
+  
+  void Autonomous() {
+    //We haven't programmed autonomous yet
+    
+    myRobot.SetSafetyEnabled(false);
+  }
+  
+  // This code runs during the tele-operated period
+  // Runs the motors with tank steering
+  // Also runs the ball pickup motor when the tigger is pressed
+  
+  void OperatorControl() {
+    
+    myRobot.SetSafetyEnabled(false);
+    
+    float scaleFactor; // Factor to scale the joystick values by
+    float leftpow,rightpow; // Power for the left and right motors
+    
+    while (IsOperatorControl()) {
+      
+      if (left.GetRawButton(1)) // if the left trigger is held
+        scaleFactor = 1.0; // TURBO MODE
+      else
+        scaleFactor = 0.5; // else let us drive precisely
+      
+      //set the power for the to the Y-axes of the Joystick multiplied by the scale factor
+      leftpow = left.getY()*scaleFactor;
+      rightpow = right.getY()*scaleFactor;
+      
+      if (right.GetRawButton(1))
+        leftpow = rightpow; // If right trigger is held, let us drive straight
+      
+      myRobot.TankDrive(leftpow,rightpow); // drive tank style
+      
+      // safety override for secondary driver
+      if (func.getRawButton(5)) // if button 5 on the control joystick is pressed
+        myRobot.TankDrive(0.0,0.0); // stop the motors
+      if (func.GetRawButton(1)) // if the trigger on the function joystick is pressed
+        ballMotor.Set(0.5); // run the ball pickup motor
+      else
+        ballMotor.Set(0.0); // else don't run the motor
+      
+      /* This code is for when we add the feeder to the robot
+       * However it is not ready yet so I have disabled it.
+      if (func.GetRawButton(2)) { // if button 2 is pressed
+        // move ball feeder up
+      } else if (func.GetRawButton(3)) { // else if button 3 is pressed
+        // move ball feeder down
+      } else {
+        // don't move feeder
+      } */
+      
+      Wait(0.005);        // wait for a motor update time
+    }
+  }
+  
+  /**
+   * Runs during test mode
+   */
+  void Test() {
 
-	}
+  }
 };
 
 START_ROBOT_CLASS(RobotDemo);
